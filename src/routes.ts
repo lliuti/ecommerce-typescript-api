e54150a24f1cdadb1196546c5101bd8a0e33879c
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-21 16:28:09 -0300

    Adding Auth route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "./database/middlewares/ensureAuthenticated";
+import { AuthenticateUserController } from "./useCases/AuthenticateUser/AuthenticateUserController";
 import { CreateUserController } from "./useCases/CreateUser/CreateUserController";
 import { ListUserController } from "./useCases/ListUser/ListUserController";
 
@@ -6,8 +8,10 @@ const routes = Router();
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 routes.get("/users", listUserController.handle);
 routes.post("/users", createUserController.handle);
+routes.post("/auth", authenticateUserController.handle);
 
 export { routes };
