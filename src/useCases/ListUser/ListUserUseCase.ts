9de97dd5449258a665add36d93939b7b1b60600c
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-22 22:24:33 -0300

    fixing classToClass to classToPlain and return type

--- src/useCases/ListUser/ListUserUseCase.ts
+++ src/useCases/ListUser/ListUserUseCase.ts
@@ -1,15 +1,15 @@
 import { getCustomRepository } from "typeorm";
 import { User } from "../../entities/User";
 import { UserRepository } from "../../repositories/UserRepository";
-import { classToClass, classToPlain } from "class-transformer";
+import { classToPlain } from "class-transformer";
 
 class ListUserUseCase {
-  async execute(): Promise<User[]> {
+  async execute(): Promise<Record<string, User[]>> {
     const userRepository = getCustomRepository(UserRepository);
 
     const userList = await userRepository.find();
 
-    return classToClass(userList);
+    return classToPlain(userList);
   }
 }
 
