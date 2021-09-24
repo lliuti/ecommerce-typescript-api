85ee60775da9b0d09505336f8850bcbc28eff77e
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-24 14:24:05 -0300

    Adding CORS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import "./database/index";
 import { routes } from "./routes";
 
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 export { app };
