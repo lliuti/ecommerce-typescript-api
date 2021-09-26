ff68da998ff934a1eb993da386b56dcbe519e3d2
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-26 15:19:15 -0300

    Adding Swagger
    Creating routes:
    get /users
    post /users
    
    get /products
    post /products

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import express from "express";
 import cors from "cors";
 import "./database/index";
 import { routes } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "./swagger.json";
 
 dotenv.config();
 
@@ -12,5 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 export { app };
